6d8a7c5a62790b5aef4e5ea0f1c365aa46d5558c
CL Bogart <clbogart3@outlook.com>
2022-04-26 08:39:48 -0600

    Add project files.

---
+++ StansGrocery_CB/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("StansGrocery_CB")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("HP")>
+<Assembly: AssemblyProduct("StansGrocery_CB")>
+<Assembly: AssemblyCopyright("Copyright © HP 2022")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("df15dafd-62ca-457f-9469-3a3bd06a0eca")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
---
+++ StansGrocery_CB/StansGrocery.vb
@@ -0,0 +1,45 @@
+﻿Option Explicit On
+Option Strict On
+Public Class StansGrocery
+    Dim food(255, 2) As String
+
+    Sub LoadDataFile()
+        Dim fileName As String = "C:\Users\clbog\OneDrive\Desktop\VisualBasic\StansGrocery_CB\StansGrocery_CB"
+        Dim record As String
+        Dim row As Integer
+        Dim temp() As String
+
+        FileOpen(1, fileName, OpenMode.Input)
+
+        Do Until EOF(1)
+
+            'grab item name
+            Input(1, record)
+            temp = Split(record, "$$ITM")
+            food(row, 0) = temp(1)
+
+            'grab location 
+            Input(1, record)
+            food(row, 1) = record
+
+            'grab category
+            Input(1, record)
+            food(row, 2) = record
+
+            row += 1
+        Loop
+
+        FileClose(1)
+    End Sub
+
+    Sub ListBoxDisplay()
+        For i = Me.food.GetLowerBound(0) To Me.food.GetUpperBound(0)
+            ListBox1.Items.Add($"{Me.food(i, 0)} : {Me.food(i, 1)} : {Me.food(i, 2)}")
+
+        Next
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        LoadDataFile()
+    End Sub
+End Class
